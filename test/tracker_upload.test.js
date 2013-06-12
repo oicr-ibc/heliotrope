32d6721104f65705625b45e1a646f18d3b5de5b4
Stuart Watt <stuart@morungos.com>
2013-06-12 14:47:22 -0400

    No functional changes, but refactored to better handle open/close db and timing

--- test/tracker_upload.test.js
+++ test/tracker_upload.test.js
@@ -26,43 +26,56 @@ function getFile(filename) {
 }
 
 describe('POST /studies/GPS/samples/TST001BIOXPAR1/step/recordResults/files', function() {
+
+  var db;
+
+  beforeEach(function(done) {
+    initialize.withDB("tracker", function(idb, ierr, iresult) {
+      db = idb;
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    if (db)
+      db.close();
+    db = undefined;
+    done();
+  });
+
   it('should process a VCF file', function(done){
 
     this.timeout(5000);
 
-    initialize.withDB("tracker", function(db, err, result) {
-      
-      var request = {params: {study: "GPS", step: "recordResults", identity: "TST001BIOXPAR1", role: "samples"}, user: {userId: "swatt"}};
-      var response = {};
-      var form = new Gently;
-      var endHandler;
-      form.expect(form, 'on', null, function(type, handler) {
-        if (type == 'end') {
-          endHandler = handler;
-        }
-      });
-      request.form = form;
+    var request = {params: {study: "GPS", step: "recordResults", identity: "TST001BIOXPAR1", role: "samples"}, user: {userId: "swatt"}};
+    var response = {};
+    var form = new Gently;
+    var endHandler;
+    form.expect(form, 'on', null, function(type, handler) {
+      if (type == 'end') {
+        endHandler = handler;
+      }
+    });
+    request.form = form;
 
-      setTimeout(function(){
-        should.exist(endHandler); 
-        var file = getFile(__dirname + '/resources/test_snps_out.vcf');
-        request.files = {files: [file]};
-        endHandler();
-      }, 500);
+    setTimeout(function(){
+      should.exist(endHandler); 
+      var file = getFile(__dirname + '/resources/test_snps_out.vcf');
+      request.files = {files: [file]};
+      endHandler();
+    }, 500);
 
-      tracker.postEntityStepFiles(null, db, request, response, function(db, err, result) {
-        db.close();
-        should.not.exist(err);
-        should.exist(result);
+    tracker.postEntityStepFiles(null, db, request, response, function(db, err, result) {
+      should.not.exist(err);
+      should.exist(result);
 
-        // At this stage, we ought to be able to find a good number of observations.
-        db.collection("entities", function(err, entities) {
-          entities.find({role: "observations"}).count(function(err, count) {
-            should.not.exist(err);
-            count.should.equal(17);
-            done();
-          })
-        });
+      // At this stage, we ought to be able to find a good number of observations.
+      db.collection("entities", function(err, entities) {
+        entities.find({role: "observations"}).count(function(err, count) {
+          should.not.exist(err);
+          count.should.equal(17);
+          done();
+        })
       });
     });
   });
