bb2a94d59aca72f87e486ba08fec137c8be7a0cc
Stuart Watt <stuart@morungos.com>
2013-06-18 17:53:50 -0400

    Tests for the POST requests for views and steps

--- test/tracker_admin.test.js
+++ test/tracker_admin.test.js
@@ -19,7 +19,7 @@ describe('GET request', function() {
     });
   });
 
-  describe('/admin/steps/GPS/participants/participant', function() {
+  describe('/steps/GPS/participants/participant', function() {
     it('should retrieve a step definition', function(done) {
       db.close();
       
@@ -38,7 +38,7 @@ describe('GET request', function() {
     });
   });
 
-  describe('/admin/steps/GPS/participants/enrolment', function() {
+  describe('/steps/GPS/participants/enrolment', function() {
     it('should retrieve a step definition', function(done) {
       db.close();
       
@@ -50,6 +50,111 @@ describe('GET request', function() {
         should.not.exist(err);
         should.exist(result);
         should.exist(result.data);
+
+        res.locals.passthrough.should.equal("value");
+        done();
+      });
+    });
+  });
+
+  describe('/views/GPS/participants/summary', function() {
+    it('should retrieve a view definition', function(done) {
+      db.close();
+      
+      var request = {"params": {"study" : "GPS", "role" : "participants", "view" : "summary"}, "user": {"userId": "swatt"}};
+      var response = {locals: {passthrough: "value"}};
+      tracker.getStudyView(null, db, request, response, function(db, err, result, res) {
+        db.close();
+        
+        should.not.exist(err);
+        should.exist(result);
+        should.exist(result.data);
+        should.exist(result.data.study);
+        should.exist(result.data.view);
+
+        result.data.view.name.should.equal("summary");
+        result.data.view.role.should.equal("participants");
+        should.exist(result.data.view.body);
+        
+        res.locals.passthrough.should.equal("value");
+        done();
+      });
+    });
+  });
+});
+
+describe('POST request', function() {
+
+  var db;
+
+  beforeEach(function(done) {
+    initialize.withDB("tracker", function(idb, ierr, iresult) {
+      db = idb;
+      done();
+    });
+  });
+
+  describe('/steps/GPS', function() {
+    it('should create a step definition', function(done) {
+      db.close();
+      
+      var request = {"params": {"study" : "GPS"}, "user": {"userId": "swatt"}};
+      var response = {locals: {passthrough: "value"}};
+      request["body"] = {
+        "data": {
+          "name": "interviewing",
+          "appliesTo": "participants",
+          "label": { "default" : "Interviewing" },
+          "isRepeatable": false,
+          "showSummary": true,
+          "weight": 20,
+          "fields": {
+            "interviewDate" : {
+              "controlType": "date",
+              "isRequired": true,
+              "type": "Date",
+              "label": { "default" : "Interview date" }
+            }
+          }
+        }
+      };
+
+      tracker.postStudyStep(null, db, request, response, function(db, err, result, res) {
+        db.close();
+        
+        should.not.exist(err);
+        should.exist(result);
+        result.should.equal("/admin/steps/GPS/participants/interviewing");
+
+        res.locals.passthrough.should.equal("value");
+        done();
+      });
+    });
+  });
+
+
+  describe('/views/GPS', function() {
+    it('should create a view definition', function(done) {
+      db.close();
+      
+      var request = {"params": {"study" : "GPS"}, "user": {"userId": "swatt"}};
+      var response = {locals: {passthrough: "value"}};
+      request["body"] = {
+        "data": {
+          "name": "report",
+          "role": "participants",
+          "label": { "default" : "Interviewing" },
+          "weight": 3.1415927,
+          "body": "<p>Testing</p>"
+        }
+      };
+
+      tracker.postStudyView(null, db, request, response, function(db, err, result, res) {
+        db.close();
+        
+        should.not.exist(err);
+        should.exist(result);
+        result.should.equal("/admin/views/GPS/participants/report");
 
         res.locals.passthrough.should.equal("value");
         done();
