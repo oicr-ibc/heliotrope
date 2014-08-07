90b0dba0f1c45ba80f65ebc87dbf1f45f0c9bb6e
Stuart Watt <stuart@morungos.com>
2014-08-07 17:55:32 -0400

    Improved the build documentation

--- etc/scripts/build.sh
+++ etc/scripts/build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 # This script initializes the knowledge base. Best run when you have plenty of time
-# to spare. 
+# to spare.
 
-perl -I. boot.pl
-perl -I. ensembl.pl
-perl -I. cosmic.pl
-perl -I. entrez.pl
-perl -I. cgc.pl
+export TMPDIR=
+
+export HELIOTROPE_DATABASE_NAME=heliotrope_new
+export HELIOTROPE_DATABASE_SERVER=localhost:27017
+export HELIOTROPE_DATABASE_USERNAME=
+export HELIOTROPE_DATABASE_PASSWORD=
+
+export HELIOTROPE_CACHE_ROOT=/Volumes/LaCie/data/heliotrope
+
+export VEP_HOME=
+export VEP_CACHE_DIRECTORY=
+
+perl boot.pl --force
+perl ensembl.pl --force
+perl cosmic.pl --force
