362536725c33ee228b2b35960410d4f7566e8ee2
Stuart Watt <stuart@morungos.com>
2014-08-15 12:47:57 -0400

    Documented the package installation process somewhat better

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@ npm install
 bower install
 node node_modules/gulp/bin/gulp.js build-all
 node node_modules/gulp/bin/gulp.js dist
-vagrant up
+vagrant up --no-provision
 ansible-playbook -v -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.vagrant.d/insecure_private_key -u vagrant packaging.yml
+
+
+## Testing the package really works best when you can install dependencies as well as a .deb
+sudo apt-get install gdebi-core
+sudo gdebi -n /vagrant/.tmp/heliotrope_0.1.0_amd64.deb
