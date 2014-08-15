7ed67e6bf34050dfee527bfcebe62fa1043aa1cb
Stuart Watt <stuart@morungos.com>
2014-08-15 10:43:22 -0400

    Improved packaging, and especially its use of version detection. Jinja2 is a bit broken, really, when you can't add filters and stuff (which you can't in Ansible)

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ npm install
 bower install
 node node_modules/gulp/bin/gulp.js build-all
 node node_modules/gulp/bin/gulp.js dist
+vagrant up
+ansible-playbook -v -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.vagrant.d/insecure_private_key -u vagrant packaging.yml
