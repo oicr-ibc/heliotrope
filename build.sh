ed796cb2de19333681bf0d81053bfa8fa048716b
Stuart Watt <stuart@morungos.com>
2014-08-15 17:30:27 -0400

    Fix the hang in postinst.

--- build.sh
+++ build.sh
@@ -7,5 +7,11 @@ ansible-playbook -v -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_i
 
 
 ## Testing the package really works best when you can install dependencies as well as a .deb
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-org
+
 sudo apt-get install gdebi-core
 sudo gdebi -n /vagrant/.tmp/heliotrope_0.1.0_amd64.deb
