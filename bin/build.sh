1e8d7dd55313391324b6af4b35d5643ac80463a8
Stuart Watt <stuart@morungos.com>
2014-08-22 12:47:45 -0400

    Removed environment variables from the build.sh script. They're no longer used, see the config files for a replacement (which actually still allows environment variables, just for different uses).

--- bin/build.sh
+++ bin/build.sh
@@ -18,18 +18,6 @@
 # This script initializes the knowledge base. Best run when you have plenty of time
 # to spare.
 
-export TMPDIR=
-
-export HELIOTROPE_DATABASE_NAME=heliotrope_new
-export HELIOTROPE_DATABASE_SERVER=localhost:27017
-export HELIOTROPE_DATABASE_USERNAME=
-export HELIOTROPE_DATABASE_PASSWORD=
-
-export HELIOTROPE_CACHE_ROOT=/Volumes/LaCie/data/heliotrope
-
-export VEP_HOME=
-export VEP_CACHE_DIRECTORY=
-
 perl boot.pl --force
 perl ensembl.pl --force
 perl cosmic.pl --force
