98e3657dabf0734164545703e94ac75e356fe337
Stuart Watt <stuart@morungos.com>
2014-09-19 12:38:40 -0400

    Added some installer information. You can now use "cpanm --installdeps ." to install the required dependencies

---
+++ lib/Heliotrope.pm
@@ -0,0 +1,40 @@
+package Heliotrope;
+
+use common::sense;
+
+our $VERSION = '0.10';
+
+1;
+
+=head1 NAME 
+
+Heliotrope - An integrated clinical genomics application
+
+=head1 DESCRIPTION
+
+Heliotrope is a set of command-line scripts and a web application
+to help with clinical interpretation of genomic results from
+next generation sequencing.
+
+=head1 AUTHORS
+
+Stuart Watt C<stuart@morungos.com>
+
+Repository available at: L<https://github.com/oicr-ibc/heliotrope>.
+
+=head1 LICENSE
+
+Copyright 2014(c) The Ontario Institute for Cancer Research. All rights reserved.
+
+This program and the accompanying materials are made available under the terms of the GNU Public
+License v3.0. You should have received a copy of the GNU General Public License along with this
+program. If not, see <http://www.gnu.org/licenses/>.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
